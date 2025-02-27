0c76e1a949df9f4bf8af173f14d0e0418852f5a3
thimirasadeesha <thimira@maximimpressions.com>
2025-02-27 17:01:54 +0530

    Add Db connection with  loggers

--- api/api.go
+++ api/api.go
@@ -2,19 +2,20 @@ package api
 
 import (
 	"GoPorject/config"
-	"GoPorject/log"
+	_ "GoPorject/log"
 	"database/sql"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
 )
 
-func StartServer(cfg *config.Config, db *sql.DB) error {
+func StartServer(cfg *config.Config, db *sql.DB, r *gin.Engine) error {
 	if cfg.ServerPort == "" {
 		log.Fatal("Server port not provided in config.")
 	}
 
+	// Use Gin's method to run the server
 	address := fmt.Sprintf(":%s", cfg.ServerPort)
-	logger.Info("Starting server on " + address)
-	return http.ListenAndServe(address, nil)
+	log.Printf("Starting server on %s\n", address) // You can use your logger here if you prefer
+	return r.Run(address)
 }
--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"GoPorject/config"
 	"GoPorject/route"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
+	cfg := config.LoadConfig()
+	db := config.Connect(cfg)
+	defer db.Close()
 	r := gin.Default()
-	gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.DebugMode)
+	err := r.SetTrustedProxies([]string{"127.0.0.1", "192.168.23.230"})
+	if err != nil {
+		log.Fatalf("Error setting trusted proxies: %v", err)
+	}
 	route.SetupRoutes(r)
-	r.Run(":8080")
+	r.Run(":4000")
 }
